478d1980619f87f83e09103f8885ddf9818a7930
alexkasema <lexxmulwa@gmail.com>
2023-12-17 02:42:29 -0800

    sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the
+ *	Quick sort algorithm.
+ * @array: Array to be sorted.
+ * @size: Size of the array.
+ * Returns: Void.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_array(array, 0, size - 1, size);
+}
+
+/**
+ * partition_array - splits an array into partitions.
+ * @array: Array to partition.
+ * @low: first index
+ * @high: last index
+ * @size: size of the array
+ * Returns: The index of the pivot element after partitioning
+ */
+
+int partition_array(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1, j = low;
+	int tmp = 0;
+
+	for (; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (array[i] != array[j])
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[i + 1] != array[high])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_array - Array to be sorted by partitioning.
+ * @array: Array to be sorted.
+ * @low: first index
+ * @high: last index
+ * @size: size of the array
+ * Returns: void
+ */
+
+void quick_sort_array(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition_array(array, low, high, size);
+
+		quick_sort_array(array, low, pivot - 1, size);
+		quick_sort_array(array, pivot + 1, high, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -23,6 +23,10 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+int partition_array(int *array, int low, int high, size_t size);
+void quick_sort_array(int *array, int low, int high, size_t size);
 
 
 void print_array(const int *array, size_t size);
